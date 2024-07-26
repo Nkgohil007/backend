47980e411b909116a30a7d4b5826fa7aabc4c4d7
Nkgohil007 <nikhil.gohil@mindinventory.com>
2024-07-26 19:12:51 +0530

    feat(): update utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import { config } from 'dotenv';
 
 import { connectDB } from './db/index.js';
+import express from 'express';
 config()
 
+const app = express()
 
-connectDB()
+connectDB().then(
+    ()=>{
+        app.listen(process.env.PORT || 8000 ,()=>{
+            console.log("SERVER IS RUNNING", process.env.PORT)
+        })
+    }
+).catch((error)=>{console.log("MONGO connection failed",error);})
 
 
