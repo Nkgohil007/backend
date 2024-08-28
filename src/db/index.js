e9b36b4fcc29adca01b449ab45879455db31a538
Nkgohil007 <nikhil.gohil@mindinventory.com>
2024-08-28 14:29:09 +0530

    feat(backend): continue with backend

--- src/db/index.js
+++ src/db/index.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
-export const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log("MONGOOSE connected",JSON.stringify(connectionInstance.connection.host));
-  } catch (error) {
-    console.error("ERROR::", error);
-    throw error;
-  }
-};
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
